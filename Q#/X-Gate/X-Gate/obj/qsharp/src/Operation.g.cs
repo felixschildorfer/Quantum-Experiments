77ae190da77173c0cb78cc725be9ebc26e1f5447
felixschildorfer <32631391+felixschildorfer@users.noreply.github.com>
2018-06-06 13:01:54 +0200

    Summarized Commits

--- Q#/X-Gate/X-Gate/obj/qsharp/src/Operation.g.cs
+++ Q#/X-Gate/X-Gate/obj/qsharp/src/Operation.g.cs
@@ -4,8 +4,8 @@ using Microsoft.Quantum.Primitive;
 using Microsoft.Quantum.Simulation.Core;
 using Microsoft.Quantum.MetaData.Attributes;
 
-[assembly: OperationDeclaration("Quantum.X_Gate", "Set (desired : Result, q1 : Qubit) : ()", new string[] { }, "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs", 162L, 7L, 5L)]
-[assembly: OperationDeclaration("Quantum.X_Gate", "X_Gate (count : Int, initial : Result) : (Int, Int)", new string[] { }, "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs", 407L, 20L, 5L)]
+[assembly: OperationDeclaration("Quantum.X_Gate", "Set (desired : Result, q1 : Qubit) : ()", new string[] { }, "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs", 162L, 7L, 5L)]
+[assembly: OperationDeclaration("Quantum.X_Gate", "X_Gate (count : Int, initial : Result) : (Int, Int)", new string[] { }, "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs", 407L, 20L, 5L)]
 #line hidden
 namespace Quantum.X_Gate
 {
@@ -47,12 +47,12 @@ namespace Quantum.X_Gate
                 try
                 {
                     var (desired,q1) = _args;
-#line 10 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 10 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     var current = M.Apply<Result>(q1);
-#line 12 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 12 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     if ((desired != current))
                     {
-#line 14 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 14 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                         MicrosoftQuantumPrimitiveX.Apply(q1);
                     }
 
@@ -137,38 +137,36 @@ namespace Quantum.X_Gate
                 try
                 {
                     var (count,initial) = _args;
-#line 23 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 23 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     var numOnes = 0L;
-#line 24 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 24 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     var qubits = Allocate.Apply(1L);
-#line 26 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 26 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     foreach (var test in new Range(1L, count))
                     {
-#line 28 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 28 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                         Set.Apply((initial, qubits[0L]));
-#line 30 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 30 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                         MicrosoftQuantumPrimitiveX.Apply(qubits[0L]);
-#line 31 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
-                        MicrosoftQuantumPrimitiveX.Apply(qubits[0L]);
-#line 32 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 31 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                         var res = M.Apply<Result>(qubits[0L]);
                         // Count the number of ones we saw:
-#line 35 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 34 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                         if ((res == Result.One))
                         {
-#line 37 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 36 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                             numOnes = (numOnes + 1L);
                         }
                     }
 
-#line 40 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 39 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     Set.Apply((Result.Zero, qubits[0L]));
 #line hidden
                     Release.Apply(qubits);
 #line hidden
                     __result__ = ((count - numOnes), numOnes);
                     // Return number of times we saw a |0> and number of times we saw a |1>
-#line 43 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\Q#\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
+#line 42 "C:\\Users\\felix.schildorfer\\OneDrive - Avanade\\Education\\GitHub\\Quantum-Experiments\\Quantum Experiments\\X-Gate\\X-Gate\\Operation.qs"
                     return __result__;
                 }
                 finally
